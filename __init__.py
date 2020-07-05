3699e25da9ec02f067dc47121f16fc11bc27bc22
ksaisudha24 <saisudha2408@gmail.com>
2020-07-05 21:03:33 +0530

    connect front and back end

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ def create_app(test_config=None):
 
     @app.route('/')
     def home():
-        return render_template('base.html')
+        return render_template('home.html')
 
     from . import db
     db.init_app(app)
