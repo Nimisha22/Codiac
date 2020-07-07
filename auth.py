7b9b55c76a7e02a02d927edb516a1d11b28bf844
ksaisudha24 <saisudha2408@gmail.com>
2020-07-07 16:00:23 +0530

    removed register front end

--- auth.py
+++ auth.py
@@ -36,7 +36,7 @@ def register():
 
         flash(error)
 
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
