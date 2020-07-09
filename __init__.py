db2fd6dbce744d15cf5b5a27cf714722940f8ca0
ksaisudha24 <saisudha2408@gmail.com>
2020-07-09 16:42:27 +0530

    connected front and back ends

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
 
     try:
         os.makedirs(app.instance_path)
@@ -22,13 +22,13 @@ def create_app(test_config=None):
     def home():
         return render_template('home.html')
 
-    from . import db
+    from cipher import db
     db.init_app(app)
 
-    from . import auth
+    from cipher import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
+    from cipher import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
     
