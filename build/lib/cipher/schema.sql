c95f1e11bfa4d6d8a171e67663fd4b053e13288b
ksaisudha24 <saisudha2408@gmail.com>
2020-08-22 10:37:39 +0530

    gitignore

---
+++ build/lib/cipher/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS postcomments;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  --email string UNIQUE NOT NULL,
+  username string UNIQUE NOT NULL,
+  password string NOT NULL
+);
+
+CREATE TABLE post (
+  topic_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE postcomments (
+    topic_id INTEGER NOT NULL,
+    --comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    comments TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
